e7eed2f764c39cdcfdfa9e14926573b43ac2bff3
Codemis <johnathan@still-water.com>
2011-04-07 19:24:45 -0700

    Adding gitignore.

---
+++ SanGabrielUnionChurch.xcdatamodeld/Feed.h
@@ -0,0 +1,26 @@
+//
+//  Feed.h
+//  SanGabrielUnionChurch
+//
+//  Created by Johnathan Pulos on 4/6/11.
+//  Copyright 2011 SGUC. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+
+
+@interface Feed :  NSManagedObject  
+{
+}
+
+@property (nonatomic, retain) NSString * author;
+@property (nonatomic, retain) NSString * title;
+@property (nonatomic, retain) NSString * feedType;
+@property (nonatomic, retain) NSDate * publishedOn;
+@property (nonatomic, retain) NSString * summary;
+@property (nonatomic, retain) NSString * feedLink;
+
+@end
+
+
+
---
+++ SanGabrielUnionChurch.xcdatamodeld/Feed.m
@@ -0,0 +1,21 @@
+// 
+//  Feed.m
+//  SanGabrielUnionChurch
+//
+//  Created by Johnathan Pulos on 4/6/11.
+//  Copyright 2011 SGUC. All rights reserved.
+//
+
+#import "Feed.h"
+
+
+@implementation Feed 
+
+@dynamic author;
+@dynamic title;
+@dynamic feedType;
+@dynamic publishedOn;
+@dynamic summary;
+@dynamic feedLink;
+
+@end
